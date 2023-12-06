f2792149aef5af3bc551f5d2c07598c5d1437eea
Hassan Mahmood <hassanmahmood@live.co.uk>
2023-12-06 19:12:50 +0000

    Create form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,23 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ('name', 'description', 'quantity')
+        widgets = {
+            'name': forms.TextInput(),
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput(),
+        }
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        description = cleaned_data.get('description')
+        quantity = cleaned_data.get('quantity')
+        
+    
